17c0bc65af9a658ba63afa16b169c859da10dc24
RafaEscobar <rafael.eg.code@gmail.com>
2023-02-06 01:00:52 -0600

    Corrección de caja en -AttendancePage-

--- src/pages/attendances/AttendancePage.js
+++ src/pages/attendances/AttendancePage.js
@@ -6,7 +6,7 @@ export const AttendancePage = () => {
   return (
     <View>
         <Text>Tabla de Asistencias</Text>
-        <AttendanceComponent nombre='Rafael Escobar' noControl='222111269' entrada='08:00' salida='14:00' horas='06' />
+        <AttendanceComponent nombre='Rafael Escobar' puesto='Gerente' entrada='08:00' salida='14:00' horas='06' />
     </View>
   )
 }
