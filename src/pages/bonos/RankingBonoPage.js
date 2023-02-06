8a53b25b74d3290dbd2e00b1561420a2f7632853
RafaEscobar <rafael.eg.code@gmail.com>
2023-02-06 02:45:54 -0600

    Rediseño en caja para -RankingBonoPage-

--- src/pages/bonos/RankingBonoPage.js
+++ src/pages/bonos/RankingBonoPage.js
@@ -4,6 +4,7 @@ import { StyleSheet } from 'react-native';
 
 // Props para navigator
 import { StackScreenProps } from '@react-navigation/stack';
+import { paletaColors } from '../../themes/appTheme';
 
 let url_Provicional='https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80';
 
@@ -13,8 +14,10 @@ export const RankingBonoPage = ({navigation}: Props) => {
   return (
     <View>
         <Text>Asignar horas extra</Text>
-        <View style={styless.box}>
-          <Text style={{fontSize: 19}}>{1}. Rafael Escobar   Días puntual: {10}</Text>
+        <View style={{backgroundColor: paletaColors.secondary, marginTop: 20, marginBottom: 10, height: 30, justifyContent: 'center'}}>
+          <View style={styless.box}>
+            <Text style={{fontSize: 19}}>{1}. Rafael Escobar   Días puntual: {10}</Text>
+          </View>
         </View>
     </View>
   )
@@ -24,8 +27,6 @@ const styless = StyleSheet.create({
 
   box: {
     marginLeft: 30,
-    marginTop: 25,
-    marginBottom: 10,
   }
 
 });
