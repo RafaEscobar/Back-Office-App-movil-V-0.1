ca0ab6eaa8e943c2cef88c568e24abec33fe72e8
RafaEscobar <rafael.eg.code@gmail.com>
2023-02-06 01:45:48 -0600

    Correccion para caja en -UserBonosPage-

--- src/components/UserBonoComponent.js
+++ src/components/UserBonoComponent.js
@@ -5,25 +5,24 @@ import { styles } from '../themes/appTheme'
 interface Props {
     img_path: string,
     nombre: string,
-    fecha: string,
-    noControl: string,
-    hrsAsignadas: string
+    turno: string,
+    bono: string
 }
 
-export const HoursComponent = ({ img_path, nombre, fecha, noControl, hrsAsignadas }: Props) => {
+export const UserBonoComponent = ({ img_path, nombre, turno, bono }: Props) => {
   return (
     <Box style={styles.boxCenter}>
         <Box style={styles.boxAccess}>
-          <Box style={{marginRight: 20}}>
+          <Box style={{marginLeft: 8, marginRight: 20}}>
             <Avatar bg="cyan.500" source={{ uri: img_path}}></Avatar>
           </Box>
-          <Box style={{marginRight: 20}}>
+          <Box style={{marginRight: 45}}>
             <Text style={{marginBottom: 7}}>{nombre}</Text>
-            <Text>{fecha}</Text>
+            <Text>Turno: {turno}</Text>
           </Box>
           <Box>
-            <Text style={{marginBottom: 7}}>Id: {noControl}</Text>
-            <Text>Asignadas: {hrsAsignadas}hrs</Text>
+            <Text style={{marginBottom: 7}}>Tipo de bono:</Text>
+            <Text>{bono}</Text>
           </Box>
         </Box>
     </Box>
--- src/pages/bonos/UserBonoPage.js
+++ src/pages/bonos/UserBonoPage.js
@@ -1,6 +1,6 @@
 import { Text, View } from 'native-base'
 import React from 'react'
-import { HoursComponent } from '../../components/HoursComponent'
+import { UserBonoComponent } from '../../components/UserBonoComponent'
 
 let url_Provicional='https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80';
 
@@ -8,7 +8,7 @@ export const UserBonoPage = () => {
   return (
         <View>
             <Text>Registro de usuarios con horas extra asignadas</Text>
-            <HoursComponent img_path={url_Provicional} nombre='Rafael Escobar' fecha='12/12/2023' noControl='222111' hrsAsignadas='8' />
+            <UserBonoComponent img_path={url_Provicional} nombre='Rafael Escobar' turno='Primero' bono='Mensual' />
         </View>
     )
 }
