e773fa552ed0a900cdc4e5f9b4f2f655890009f0
RafaEscobar <rafael.eg.code@gmail.com>
2023-02-05 20:36:15 -0600

    Instalacion de Stack

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'react-native-gesture-handler'
 import { NativeBaseProvider, Box, Text } from 'native-base'
 import { NavigationContainer } from '@react-navigation/native'
 
