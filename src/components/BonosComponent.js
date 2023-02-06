63eba9a37caf3ae63d9dc41c4ef922b17c86c56b
RafaEscobar <rafael.eg.code@gmail.com>
2023-02-06 02:22:55 -0600

    Correccion para caja en -TypeBonosPage-

---
+++ src/components/BonosComponent.js
@@ -0,0 +1,37 @@
+import { Avatar, Box } from 'native-base';
+import React from 'react'
+import { StyleSheet, Text } from 'react-native';
+import { styles } from '../themes/appTheme';
+
+interface Props {
+    nombre: string,
+    descripcion: string,
+  }
+
+export const BonosComponent = ({nombre, descripcion }: Props) => {
+  return (
+<Box style={styles.boxCenter}>
+        <Box style={styles.boxAttendance}>
+            <Box style={styless.boxPrincipal}>
+                <Text style={{fontSize: 18, fontWeight: 'bold', marginBottom: 5}}>{nombre}</Text>
+            </Box>
+            <Box style={styless.boxSecond}>
+                <Box style={styless.box}>
+                  <Text>{descripcion}</Text>
+                </Box>
+            </Box>
+        </Box>
+    </Box>
+    )
+}
+
+const styless = StyleSheet.create({
+    boxImg: {
+      marginRight: 15,
+    },
+    boxes: {
+      flex:3, 
+      marginRight: 15,
+    },
+  });
+  
--- src/pages/bonos/TypeBonosPage.js
+++ src/pages/bonos/TypeBonosPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {View, Text} from 'native-base'
+import { BonosComponent } from '../../components/BonosComponent'
 
 export const TypeBonosPage = () => {
   return (
         <View>
-            <Text>Holaaaaa</Text>
+            <Text>Tipos de bonos</Text>
+            <BonosComponent nombre='Mensual' descripcion='recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' />
         </View>
     )
 }
