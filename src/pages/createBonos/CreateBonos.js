09fb426d99044fd493ce477d211bf21393744428
RafaEscobar <rafael.eg.code@gmail.com>
2023-03-15 18:52:37 -0600

    Formularios agregados

--- src/pages/createBonos/CreateBonos.js
+++ src/pages/createBonos/CreateBonos.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import { Stack, Input, Button, FormControl, Radio, Box, TextArea } from 'native-base'
 import {View, StyleSheet, ScrollView,} from 'react-native'
-
+import { useFormik } from 'formik'
 
 export const CreateBonos = () => {
+    const {values, setFieldValue} = useFormik({
+        initialValues: {
+            bono: '',
+            monto: '',
+            descripcion: '',
+        },
+    });
   return (
     <ScrollView >
         <View style={{flex:1, backgroundColor: 'white', alignItems: 'center', }}>
             <FormControl style={styles.box}>
                 <FormControl.Label style={styles.txt}>Bono: </FormControl.Label>
-                <Input variant="underlined" placeholder="Ingresa el nombre del bono " />
+                <Input variant="underlined" placeholder="Ingresa el nombre del bono" value={values.bono} onChangeText={text => setFieldValue('bono', text)} />
                 <FormControl.Label style={styles.txt}>Monto:</FormControl.Label>
-                <Input variant="underlined" placeholder="Ingresa el monto a pagar por el bono" />
+                <Input variant="underlined" placeholder="Ingresa el monto a pagar por el bono" value={values.monto} onChangeText={text => setFieldValue('monto', text)} />
                 <FormControl.Label style={styles.txt}>Descripción: </FormControl.Label>
-                <TextArea h={20} placeholder="Ingresa la descripción del bono" w="100%" />
+                <TextArea h={20} placeholder="Ingresa la descripción del bono" w="100%" value={values.descripcion} onChangeText={text => setFieldValue('descripcion', text)} />
                 <Button size="sm" variant="outline" style={{marginTop: 235}}>Enviar</Button>
                 <Box style={{marginBottom: 40}}></Box>
             </FormControl>
--- src/pages/createPuestos/CreatePuesto.js
+++ src/pages/createPuestos/CreatePuesto.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import { Stack, Input, Button, FormControl, Radio, Box } from 'native-base'
 import {View, StyleSheet, ScrollView,} from 'react-native'
+import { useFormik } from 'formik'
 
 export const CreatePuesto = () => {
+
+    const {values, setFieldValue} = useFormik({
+        initialValues: {
+            nombre: '',
+            salario: '',
+            estatus: 'activo',
+            rol: '',
+            area: '',
+        },
+    });
+
   return (
     <ScrollView >
         <View style={{flex:1, backgroundColor: 'white', alignItems: 'center', }}>
             <FormControl style={styles.box}>
                 <FormControl.Label style={styles.txt}>Nombre:</FormControl.Label>
-                <Input variant="underlined" placeholder="Ingresa el nombre del puesto" />
+                <Input variant="underlined" placeholder="Ingresa el nombre del puesto" value={values.nombre} onChangeText={text => setFieldValue('nombre', text)} />
                 <FormControl.Label style={styles.txt}>Salario:</FormControl.Label>
-                <Input variant="underlined" placeholder="Ingresa el salario del puesto" />
+                <Input variant="underlined" placeholder="Ingresa el salario del puesto" value={values.salario} onChangeText={text => setFieldValue('salario', text)} />
                 <FormControl.Label style={styles.txt}>Estatus:</FormControl.Label>
-                <Input variant="underlined" placeholder="Ingresa el estatus del puesto" />
+                <Input variant="underlined" placeholder="Ingresa el estatus del puesto" value={values.estatus} onChangeText={text => setFieldValue('estatus', text)} />
                 <FormControl.Label style={styles.txt}>Rol:</FormControl.Label>
-                <Input variant="underlined" placeholder="Ingresa el rol con el que cuenta el puesto" />
+                <Input variant="underlined" placeholder="Ingresa el rol con el que cuenta el puesto" value={values.rol} onChangeText={text => setFieldValue('rol', text)} />
                 <FormControl.Label style={styles.txt}>Área:</FormControl.Label>
-                <Input variant="underlined" placeholder="Ingresa el área del puesto" />
+                <Input variant="underlined" placeholder="Ingresa el área del puesto" value={values.area} onChangeText={text => setFieldValue('area', text)} />
 
                 <Button size="sm" variant="outline" style={{marginTop: 80}}>Enviar</Button>
                 <Box style={{marginBottom: 40}}></Box>
